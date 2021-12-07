79538631bbb4ca4696e2f4de61147a3b3bfefcea
ziggi <ziggi.willpower@gmail.com>
2021-12-07 15:18:06 +0100

    refactor for better code structure

--- src/main/java/com/example/BookingSystem/Controllers/BookingController.java
+++ src/main/java/com/example/BookingSystem/Controllers/BookingController.java
@@ -1,5 +1,7 @@
-package com.example.BookingSystem.Booking;
+package com.example.BookingSystem.Controllers;
 
+import com.example.BookingSystem.Models.Booking;
+import com.example.BookingSystem.Services.BookingService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/BookingSystem/Repositories/BookingRepository.java
+++ src/main/java/com/example/BookingSystem/Repositories/BookingRepository.java
@@ -1,5 +1,6 @@
-package com.example.BookingSystem.Booking;
+package com.example.BookingSystem.Repositories;
 
+import com.example.BookingSystem.Models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
