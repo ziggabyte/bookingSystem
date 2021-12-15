56f927b815fde722e4c59210965675891876e758
aJohnson <johnson9438@gmail.com>
2021-12-15 11:59:30 +0100

    frontend fixes, new files for better readability. Login and date/time formatting still WIP.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Booking from "./pages/Booking";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import CustomerPage from "./pages/CustomerPage";
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/customerpage" element={<CustomerPage />} />
         </Routes>
       </Router>
     </div>
@@ -20,3 +24,10 @@ function App() {
 }
 
 export default App;
+
+export const CustomButton = styled(Button)({
+  backgroundColor: "#364136",
+  "&:hover": {
+    backgroundColor: "#355F34",
+  },
+});
